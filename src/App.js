4ca6843fc08111780f150718f8fee038094a9c5b
Lisa Bondarenko <lisa.bond.github@gmail.com>
2022-09-07 22:57:13 +0300

    Project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import dictionary from "./dictionary.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={dictionary}
+          className="App-logo img-fluid"
+          alt="Dictionary logo"
+        />
+      </header>
+    </div>
+  );
+}
