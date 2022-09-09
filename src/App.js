7a6a80de959abd38ac0649742f7b9ded631c4a21
Lisa Bondarenko <lisa.bond.github@gmail.com>
2022-09-09 22:46:32 +0300

    Search engine setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import dictionary from "./dictionary.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={dictionary}
-          className="App-logo img-fluid"
-          alt="Dictionary logo"
-        />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={dictionary}
+            className="App-logo img-fluid"
+            alt="Dictionary logo"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by me</footer>
+      </div>
     </div>
   );
 }
